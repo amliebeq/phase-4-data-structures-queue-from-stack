a90e44864eee277e7c0dae948b35d07b7005bf1d
alex lieberman <alex.lieberman182@gmail.com>
2023-06-14 10:36:39 -0700

    commit tree 8401a41bea435783c512d14424255a020436f765

--- lib/queue_from_stack.rb
+++ lib/queue_from_stack.rb
@@ -1,3 +1,18 @@
 require_relative './stack'
 
-# your code here
+class MyQueue
+    def initialize
+        @s1 = Stack.new
+        @s2 = Stack.new
+    end
+
+    def add (value)
+        @s1.push(value)
+    end
+
+    def remove
+    end
+
+    def peek
+    end
+end
